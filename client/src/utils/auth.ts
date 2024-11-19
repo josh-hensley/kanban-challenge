20571b0459df2ad0f16b691b6067b189d3df8c25
josh-hensley <me@joshhensley.com>
2024-11-19 17:49:05 -0600

    logic for auth util implemented

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -2,29 +2,36 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 
 class AuthService {
   getProfile() {
-    // TODO: return the decoded token
+    // return the decoded token
+    return ;
   }
 
   loggedIn() {
-    // TODO: return a value that indicates if the user is logged in
+    // return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
   }
   
   isTokenExpired(token: string) {
-    // TODO: return a value that indicates if the token is expired
+    // return a value that indicates if the token is expired
+    const payload: JwtPayload = jwtDecode(token);
+    return payload.exp === 0;
   }
 
   getToken(): string {
-    // TODO: return the token
+    // return the token
+    return localStorage.getItem('id_token') || '';
   }
 
   login(idToken: string) {
-    // TODO: set the token to localStorage
-    // TODO: redirect to the home page
+    // set the token to localStorage and load homepage;
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/')
   }
 
   logout() {
-    // TODO: remove the token from localStorage
-    // TODO: redirect to the login page
+    localStorage.removeItem('id_token');
+    window.location.assign('/');
   }
 }
 
